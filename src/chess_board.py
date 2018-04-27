3afa6b29dde51fdff015ba9ee94f33aeff6c8937
benjaminbowen00 <benjibowen@hotmail.com>
2018-04-27 10:15:13 +0100

    chessboard checks if position is valid and stops duplication

--- src/chess_board.py
+++ src/chess_board.py
@@ -6,10 +6,20 @@ class ChessBoard:
         self.pieces = [[None] * 7 for _ in range(7)]
 
     def add(self, pawn, x_coordinate, y_coordinate, piece_color):
-        pawn.x_coordinate = x_coordinate
-        pawn.y_coordinate = y_coordinate
-        pawn.piece_color = piece_color
-        self.pieces[x_coordinate][y_coordinate] = pawn
+        if self.pieces[y_coordinate][x_coordinate] is None:
+            pawn.x_coordinate = x_coordinate
+            pawn.y_coordinate = y_coordinate
+            pawn.piece_color = piece_color
+            self.pieces[y_coordinate][x_coordinate] = pawn
+        else:
+            pawn.x_coordinate = -1
+            pawn.y_coordinate = -1
+            pawn.piece_color = piece_color
 
     def is_legal_board_position(self, x_coordinate, y_coordinate):
-        raise NotImplementedError()
+        is_legal_position = True
+        if(x_coordinate > len(self.pieces[1]) or x_coordinate < 0):
+            is_legal_position = False
+        if(y_coordinate > len(self.pieces) or y_coordinate < 0):
+            is_legal_position = False
+        return is_legal_position
